ec18a400cb214821de2cfc59261c602aa1dda6a1
Ethan <ethan@eyeballdivision.com>
2017-06-28 19:57:37 -0600

    Laid groundwork for creation of controllers and migrations

--- lib/generators/jarvis/install_generator.rb
+++ lib/generators/jarvis/install_generator.rb
@@ -3,7 +3,23 @@ module Jarvis
         class InstallGenerator < Rails::Generators::Base
             include Rails::Generators::Migration
             source_root File.expand_path("../templates", __FILE__)
-            class_option :orm
+
+            desc "Creates Jarvis initializer, controllers, views, and routes."
+            
+            def install
+                # Create initializer
+                copy_file "config/initializers/jarvis.rb", "config/initializers/jarvis.rb"
+
+                # Create controllers
+                directory "app/controllers/jarvis", "app/controllers/jarvis"
+
+                # Add routes
+                route %{
+                    scope :module => 'jarvis' do
+                        get '/jarvis/users' => 'users#show', :as => :jarvis_users
+                    end
+                }
+            end
 
             def create_migrations
                 Dir["#{self.class.source_root}/migrations/*.rb"].sort.each do |filepath|
