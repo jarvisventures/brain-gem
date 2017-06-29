7b0b64e82b9ed9f6fd7e64c54f20281f24583b05
Michael Tingey <tingeym2@gmail.com>
2017-06-29 17:52:38 -0600

    gemspec

--- app/controllers/jarvis/syncs_controller.rb
+++ app/controllers/jarvis/syncs_controller.rb
@@ -2,17 +2,14 @@ module Jarvis
   class SyncsController < ApplicationController
     def create
       # This is where we'll create users from the Brain
-      Brain.create_user(params[:user])
     end
 
     def update
       # This is where we'll update users from the Brain
-      Brain.update_user(params[:user_id], params[:update])
     end
 
     def destroy
       # This is where we'll destroy users from the Brain
-      Brain.destroy_user(params[:user_id])
     end
   end
 end
