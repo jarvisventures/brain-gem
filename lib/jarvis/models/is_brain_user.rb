8af9e341bdf3c719ededc806023c8e314794501e
Jarek Bird <jarekbird@Jareks-MacBook-Pro.local>
2017-07-20 16:59:00 -0600

    different include again

--- lib/jarvis/models/is_brain_user.rb
+++ lib/jarvis/models/is_brain_user.rb
@@ -1,22 +1,22 @@
-module Jarvis
-  module IsBrainUser
-    extend ActiveSupport::Concern
-
-    included do
-    end
-
-    module ClassMethods
-      def is_brain_user
-        include Jarvis::IsBrainUser::LocalInstanceMethods
-      end
-    end
-    module LocalInstanceMethods
-      def output
-        puts "I hope this works"
-      end
-    end
-  end
-end
+# module Jarvis
+#   module IsBrainUser
+#     extend ActiveSupport::Concern
+#
+#     included do
+#     end
+#
+#     module ClassMethods
+#       def is_brain_user
+#         include Jarvis::IsBrainUser::LocalInstanceMethods
+#       end
+#     end
+#     module LocalInstanceMethods
+#       def output
+#         puts "I hope this works"
+#       end
+#     end
+#   end
+# end
 
 # module Jarvis
 #   module ActiveRecord
