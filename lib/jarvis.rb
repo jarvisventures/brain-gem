736a6ad29353dc5f1386b4b6ce98e53583e6547f
Jarek Bird <jarekbird@Jareks-MacBook-Pro.local>
2017-07-20 17:10:28 -0600

    so close

--- lib/jarvis.rb
+++ lib/jarvis.rb
@@ -4,30 +4,4 @@ require 'ancestry'
 require 'jarvis/models/is_brain_user'
 
 module Jarvis
-  module IsBrainUser
-    extend ActiveSupport::Concern
-
-    included do
-    end
-
-    module ClassMethods
-      def is_brain_user
-        include Jarvis::IsBrainUser::LocalInstanceMethods
-      end
-    end
-    module LocalInstanceMethods
-      def output
-        puts "I hope this works"
-      end
-    end
-  end
-end
-
-
-ActiveSupport.on_load(:active_record) do
-  class ActiveRecord::Base
-    def self.is_brain_user
-      include Jarvis::IsBrainUser
-    end
-  end
 end
