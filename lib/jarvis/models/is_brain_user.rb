2077902bfd4e2fa1520a6a50b07c12c8554a0064
Jarek Bird <jarekbird@Jareks-MacBook-Pro.local>
2017-07-21 00:58:53 -0600

    removed ternaies

--- lib/jarvis/models/is_brain_user.rb
+++ lib/jarvis/models/is_brain_user.rb
@@ -27,10 +27,14 @@ module Jarvis
         puts "I hope this works"
       end
         hash = self.to_json
-        hash[:company] = !self.company.blank? ? self.company.to_json : {}
-        hash[:department] = !self.department.blank? ? self.department.to_json : {}
-        hash[:division] = !self.division.blank? ? self.division.to_json : {}
-        hash[:location] = !self.location.blank? ? self.location.to_json : {}
+        unless self.company.blank?
+          hash[:company] = self.company.to_json
+        else
+          hash[:company] = {}
+        end
+        # hash[:department] = !self.department.blank? ? self.department.to_json : {}
+        # hash[:division] = !self.division.blank? ? self.division.to_json : {}
+        # hash[:location] = !self.location.blank? ? self.location.to_json : {}
         return hash
       end
     end
