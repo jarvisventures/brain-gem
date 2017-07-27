46f5e6c486b1aada5b19dd31aa2f09763f01e119
joshhaws <joshhaws0@gmail.com>
2017-07-27 16:49:54 -0600

    controller and routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   post '/jarvis', to: 'jarvis/syncs#create', as: :jarvis_create
   delete '/jarvis/:api_token', to: 'jarvis/syncs#destroy', as: :jarvis_delete
   get '/sso/:api_token', to: 'jarvis/syncs#login', as: :jarvis_sso
+
+  get '/jarvis/users/(:id)', to: 'jarvis/user#show', as: :user
+  post '/jarvis/users', to: 'jarvis/user#create', as: :users
+  put '/jarvis/users/(:id)', to: 'jarvis/user#update', as: :user
+  patch '/jarvis/users/(:id)', to: 'jarvis/user#update', as: :user
+  delete '/jarvis/users/(:id)', to: 'jarvis/user#destroy', as: :user
+
+  
 end
