4e2d6e0515a032c21eb479739ac5f2095fb23c2e
joshhaws <joshhaws0@gmail.com>
2017-07-27 16:59:41 -0600

    change to version and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   delete '/jarvis/:api_token', to: 'jarvis/syncs#destroy', as: :jarvis_delete
   get '/sso/:api_token', to: 'jarvis/syncs#login', as: :jarvis_sso
 
-  get '/jarvis/users/(:id)', to: 'jarvis/user#show', as: :user
-  post '/jarvis/users', to: 'jarvis/user#create', as: :users
-  put '/jarvis/users/(:id)', to: 'jarvis/user#update', as: :user
-  patch '/jarvis/users/(:id)', to: 'jarvis/user#update', as: :user
-  delete '/jarvis/users/(:id)', to: 'jarvis/user#destroy', as: :user
+  get '/jarvis/user', to: 'jarvis/user#show'
+  post '/jarvis/user', to: 'jarvis/user#create'
+  put '/jarvis/user', to: 'jarvis/user#update'
+  patch '/jarvis/user', to: 'jarvis/user#update'
+  delete '/jarvis/user', to: 'jarvis/user#destroy'
 
-  
+
 end
--- lib/jarvis/version.rb
+++ lib/jarvis/version.rb
@@ -1,3 +1,3 @@
 module Jarvis
-  VERSION = "0.1.59"
+  VERSION = "0.1.60"
 end
